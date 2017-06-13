d50726662918e67d01556ed3a8a1353023e6b28f
Charles Dunn <dunnc1551@gmail.com>
2017-06-13 15:35:27 -0600

    Create db and set up server to get/post to db.

---
+++ db/seeds/dev/locations.js
@@ -0,0 +1,23 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('locations').del()
+    .then(() => {
+      return knex('locations').insert([
+        {
+          name: 'Denver',
+          lat: 39.742043,
+          lng: -104.991531,
+        },
+        {
+          name: 'LA',
+          lat: 34.052235,
+          lng: -118.243683,
+        },
+        {
+          name: 'Boston',
+          lat: 42.364506,
+          lng: -71.038887,
+        },
+      ]);
+    });
+};
